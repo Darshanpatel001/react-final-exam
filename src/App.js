93360abf6c409e67da80de308a8d4c451328030d
Darshanpatel001 <darshanpatel4298@gmail.com>
2024-07-17 19:12:57 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import { Route, Routes } from "react-router-dom";
+import AdminHome from "./component/pages/AdminHome";
+import UserPage from "./component/pages/UserPage";
+import MoviesDetails from "./component/pages/MoviesDetails";
+import Navbar from "./component/Navbar/Navbar";
+
+
+
+function App() {
+  let role = "user";
+
+  if (role === "user") {
+    return (
+      <>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<UserPage />} />
+          <Route path="/:id" element={<MoviesDetails />} />
+        </Routes>
+      </>
+    )
+  }
+
+  if (role === "admin") {
+    return (
+      <>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<AdminHome />} />
+        </Routes>
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ src/rect-toolkit/api/api.js
@@ -0,0 +1,24 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import { BASE_URL, DELETE_MOVIES, GET_MOVIES,  POST_MOVIES,  UPDATE_MOVIES } from "../constant";
+
+export let get_movies = createAsyncThunk("GET_MOVIES", async (action) => {
+    let { data } = await axios.get(BASE_URL + GET_MOVIES)
+    return data;
+})
+
+export let post_movies = createAsyncThunk("POST_MOVIES", async (action) => {
+    console.log(action, "from post movies");
+    let { data } = await axios.post(BASE_URL + POST_MOVIES, action)
+    return data;
+})
+
+export let delete_movies = createAsyncThunk("DELETE_MOVIES", async (action) => {
+    let { data } = await axios.delete(BASE_URL + DELETE_MOVIES + action)
+    return data;
+})
+
+export let update_movies = createAsyncThunk("UPDATE_MOVIES", async (action) => {
+    let { data } = await axios.put(BASE_URL + UPDATE_MOVIES + action.id, action)
+    console.log(data    );
+})
